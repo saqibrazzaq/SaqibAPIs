e525fd88d744664168d65a40e134da1ec5710030
saqibrazzaq <saqibrazzaq@gmail.com>
2024-11-09 14:56:58 +0500

    comment whole addazureclients

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -35,21 +35,21 @@ var host = new HostBuilder()
       services.ConfigureInfrastructureServices(configuration);
       services.ConfigureAzureServices();
 
-      services.AddAzureClients(clientBuilder =>
-      {
-        // Register clients for each service
-        //clientBuilder.AddSecretClient(configuration.GetSection("KeyVault"));
-        //clientBuilder.AddClient<QueueClient, QueueClientOptions>((_, _, _) =>
-        //{
-        //  var connectionString = configuration["AzureWebJobsStorage"];
-        //  var queueName = Constants.MESSAGE_QUEUE_NAME;
-        //  return new QueueClient(connectionString, queueName);
-        //});
+      //services.AddAzureClients(clientBuilder =>
+      //{
+      //  // Register clients for each service
+      //  //clientBuilder.AddSecretClient(configuration.GetSection("KeyVault"));
+      //  //clientBuilder.AddClient<QueueClient, QueueClientOptions>((_, _, _) =>
+      //  //{
+      //  //  var connectionString = configuration["AzureWebJobsStorage"];
+      //  //  var queueName = Constants.MESSAGE_QUEUE_NAME;
+      //  //  return new QueueClient(connectionString, queueName);
+      //  //});
 
-        // Set a credential for all clients to use by default
-        //DefaultAzureCredential credential = new();
-        //clientBuilder.UseCredential(credential);
-      });
+      //  // Set a credential for all clients to use by default
+      //  //DefaultAzureCredential credential = new();
+      //  //clientBuilder.UseCredential(credential);
+      //});
     })
     .Build();
 
